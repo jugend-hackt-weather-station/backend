2ebdd2a4023b1d67430e292841a8acde0f4db3d9
Michael König <koenigmichael2002@gmail.com>
2019-10-26 13:36:35 +0200

    Get database working

--- index.js
+++ index.js
@@ -1,14 +1,68 @@
 const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const Sequelize = require("sequelize");
+const entry = require("./model/entry.js");
 
-// Constants
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const dbOptions = {
+  name: "weatherstation",
+  host: "localhost",
+  port: "3306",
+  username: "root",
+  password: ""
+};
+
+const sequelize = new Sequelize(
+  dbOptions.name,
+  dbOptions.username,
+  dbOptions.password,
+  {
+    host: dbOptions.host,
+    port: dbOptions.port,
+    dialect: "mysql"
+  }
+);
+
+const Entry = entry.create(sequelize);
+Entry.sync({ force: false });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch(err => {
+    console.error("Unable to connect to the database:", err);
+  });
+
 const HOST = "localhost";
 const PORT = 8080;
 
-// App
-const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello world\n");
+  res.send("Hey!");
+});
+
+app.get("/status", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.get("/getAll", async (req, res) => {
+  Entry.findAll()
+    .then(data => {
+      return res.json(data);
+    })
+    .catch(e => console.log(e));
+});
+
+app.post("/submit", async (req, res) => {
+  Entry.create({ ...req.body })
+    .then(() => {
+      return res.sendStatus(200);
+    })
+    .catch(e => console.log(e));
 });
 
 app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
---
+++ model/entry.js
@@ -0,0 +1,25 @@
+const Sequelize = require("sequelize");
+const DataTypes = Sequelize.DataTypes;
+
+module.exports.create = sequelize => {
+  class Entry extends Sequelize.Model {}
+  Entry.init(
+    {
+      altitude: {
+        type: DataTypes.DECIMAL(9, 6)
+      },
+      latitude: {
+        type: DataTypes.DECIMAL(9, 6)
+      },
+      temperature: {
+        type: DataTypes.DECIMAL(6, 2)
+      }
+    },
+    {
+      sequelize,
+      tableName: "entry",
+      freezeTableName: true
+    }
+  );
+  return Entry;
+};
